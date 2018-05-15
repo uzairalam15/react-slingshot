8ba6381f4ef5f04dfd21ad6d9601b4c6ec565e4d
Uzair Alam <uzair.alam@venturedive.com>
2018-05-15 12:00:07 +0500

    cleaned project

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,8 @@
 /* eslint-disable import/no-named-as-default */
 import { NavLink, Route, Switch } from "react-router-dom";
 
-import AboutPage from "./AboutPage";
-import FuelSavingsPage from "./containers/FuelSavingsPage";
-import HomePage from "./HomePage";
-import NotFoundPage from "./NotFoundPage";
+
+import NotFoundPage from "./NotFoundPage"
 import PropTypes from "prop-types";
 import React from "react";
 import { hot } from "react-hot-loader";
@@ -13,24 +11,28 @@ import { hot } from "react-hot-loader";
 // version of hot reloading won't hot reload a stateless
 // component at the top-level.
 
+
+// example usage: 
+// <div>
+// <NavLink exact to="/" activeStyle={activeStyle}>Home</NavLink>
+// {' | '}
+// <NavLink to="/fuel-savings" activeStyle={activeStyle}>Demo App</NavLink>
+// {' | '}
+// <NavLink to="/about" activeStyle={activeStyle}>About</NavLink>
+// </div>
+// <Switch>
+// <Route exact path="/" component={HomePage} />
+// <Route path="/fuel-savings" component={FuelSavingsPage} />
+// <Route path="/about" component={AboutPage} />
+// <Route component={NotFoundPage} />
+// </Switch>
+
 class App extends React.Component {
   render() {
     const activeStyle = { color: 'blue' };
     return (
       <div>
-        <div>
-          <NavLink exact to="/" activeStyle={activeStyle}>Home</NavLink>
-          {' | '}
-          <NavLink to="/fuel-savings" activeStyle={activeStyle}>Demo App</NavLink>
-          {' | '}
-          <NavLink to="/about" activeStyle={activeStyle}>About</NavLink>
-        </div>
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/fuel-savings" component={FuelSavingsPage} />
-          <Route path="/about" component={AboutPage} />
-          <Route component={NotFoundPage} />
-        </Switch>
+       <Route component={NotFoundPage} />
       </div>
     );
   }
